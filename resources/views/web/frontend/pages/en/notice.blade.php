7b0d387985a1286d5b8851c81cf3f6fd72884692
musa_nur_rahman <musanurrahman48@gmail.com>
2024-06-26 12:57:06 +0700

    add layout notice

---
+++ resources/views/web/frontend/pages/en/notice.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="id">
+    @include('web/frontend/pages/en/component/head')
+    <link rel="stylesheet" href="assets/css/comunity.css"/>
+</head>
+<body>
+@include('web/frontend/pages/en/component/header')
+
+        <div class="page page-in">
+            <div class="notice-wrapper">
+                <div class="notice-title">
+                    <p>Notice</p>
+                </div>
+                <div class="notice-list">
+                    
+                </div>
+            </div>
+        </div>
+
+        @include('web/frontend/pages/en/component/footer')
+    </body>
+</html>
