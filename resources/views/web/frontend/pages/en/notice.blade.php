a008aeb087a31ee4617244b55412d57e200941b4
musa_nur_rahman <musanurrahman48@gmail.com>
2024-06-26 17:02:32 +0700

    notice

--- resources/views/web/frontend/pages/en/notice.blade.php
+++ resources/views/web/frontend/pages/en/notice.blade.php
@@ -12,7 +12,20 @@
                     <p>Notice</p>
                 </div>
                 <div class="notice-list">
-                    
+                    <ul>
+                        <li><a class="data" href="#">
+                            <div class="number">
+                                <p>1</p>
+                            </div>
+                            <div class="title">
+                                <p>Samchully Networks, 2023 Opening Ceremony</p>
+                            </div>
+                            <div class="date">
+                                Jan 20, 2023
+                                <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                            </div>
+                        </li></a>
+                    </ul>
                 </div>
             </div>
         </div>
