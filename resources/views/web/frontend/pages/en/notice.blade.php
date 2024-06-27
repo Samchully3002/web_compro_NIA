d3a512eb34043b0181c4db966fc80ce3773c6435
Ahmad Fiqih <fiqihwew@gmail.com>
2024-06-27 14:07:05 +0700

    fix list notice page on dekstop and mobile

--- resources/views/web/frontend/pages/en/notice.blade.php
+++ resources/views/web/frontend/pages/en/notice.blade.php
@@ -11,23 +11,166 @@
                 <div class="notice-title">
                     <p>Notice</p>
                 </div>
-                <div class="notice-list">
-                    <ul>
-                        <li><a class="data" href="#">
-                            <div class="number">
-                                <p>1</p>
-                            </div>
-                            <div class="title">
-                                <p>Samchully Networks, 2023 Opening Ceremony</p>
-                            </div>
-                            <div class="date">
-                                Jan 20, 2023
-                                <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
-                            </div>
-                        </li></a>
-                    </ul>
-                </div>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>1</span>
+                        <p>Samchully Networks, 2023 Opening Ceremony</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Jan 2, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>2</span>
+                        <p>PT NIA, 2023 Employee Meeting  and New Year Opening Ceremony</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Jan 20, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>3</span>
+                        <p>Samchully Networks, 2023’s Business Trip for Preparing System of the Master Plan and Basic Design for the Pilot Operation and Modernization of Bus Terminals in Indonesia</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Feb 6, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>4</span>
+                        <p>PT NIA, Hold System Training with Samchully Networks</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Feb 20, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>5</span>
+                        <p>PT NIA,  The Second Invitational Training at South Korea</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Jul 18, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>6</span>
+                        <p>PT NIA, Participated in the Transfer Knowledge Conference </p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Aug 25, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>7</span>
+                        <p>PT NIA, Indonesia Bus Terminal Modernization Project "Smart Terminal" Opening Ceremony</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Sep 13, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>8</span>
+                        <p>The 4th Korea – ASEAN Business Model Competition for SDGs 2023</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Oct 31, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>9</span>
+                        <p>PT NIA, Attended Focus Group Discussion (FGD) by the Directorate General of Land Transportation</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Nov 7, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>10</span>
+                        <p>PT NIA, Received an Award as the Best ODA Project at the 2023 Global Korea Forum (GKF)</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Nov 21, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>11</span>
+                        <p>PT NIA, Webcash and IBK Visit</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Nov 22, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>12</span>
+                        <p>PT NIA, Celebration of The 2nd Anniversary and Office Relocation</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Dec 10, 2023</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>13</span>
+                        <p>Revitalized, President Jokowi Inaugurates Leuwipanjang Terminal </p>
+                    </div>
+                    <div class="notice-date">
+                        <span>Feb 3, 2024</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>14</span>
+                        <p>CEO of PT Networks Indonesia Aku Speaks at 'Korea-Indonesia Land, Infrastructure, & Transportation Industry Business Matching 2024' Event</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>May 29, 2024</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
+                <a class="notice-list" href="#">
+                    <div class="notice-content">
+                        <span>15</span>
+                        <p>NIA Signs Cooperation Agreement with Notary Derry Tandoyo</p>
+                    </div>
+                    <div class="notice-date">
+                        <span>May 25, 2024</span>
+                        <img src="../../assets/images/icon/arrow-black.svg" class="language_flag">
+                    </div>
+                </a>
             </div>
+            <div class="imx">
+                    <div class="pagination">
+                        <a href="#" class="arrow">&laquo;</a>
+                        <a href="media.html" class="active">1</a>
+                        <a href="media_page2.html">2</a>
+                        <a href="media_page3.html">3</a>
+                        <a href="#" class="arrow">&raquo;</a>
+                    </div>
+                </div>
         </div>
 
         @include('web/frontend/pages/en/component/footer')
